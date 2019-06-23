84711f38cfa35e3cc3ed8192152e07a592755e11
qiyue0421 <1831545391@qq.com>
2019-06-23 23:27:36 +0800

    request库的基本用法

---
+++ request_usage.py
@@ -0,0 +1,72 @@
+import requests
+import re
+
+"""GET请求"""
+"""基本用法
+r = requests.get('http://www.baidu.com')  # 使用get()方法请求网页，得到一个Response对象
+print(type(r))
+print(r.status_code)  # 状态码
+print(type(r.text))  # 响应体类型是str
+print(r.text)  # 响应体
+print(r.cookies)  # Cookies
+"""
+
+
+"""
+data = {
+    'name': 'germey',
+    'age': 22
+}
+r = requests.get('http://httpbin.org/get', params=data)  # 将信息数据存入字典，传给params参数构造请求链接
+print(r.text)
+print(type(r.text))  # str（JSON格式）
+print(r.json())  # 使用json()方法将JSON格式的字符串转化为字典
+print(type(r.json()))  # dict
+"""
+
+
+"""抓取网页
+headers = {  # 构造请求头，伪装成浏览器，否则无法抓取
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+}
+r = requests.get('https://www.zhihu.com/explore', headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)  # 构建正则表达式
+titles = re.findall(pattern, r.text)  # 匹配想要的内容
+print(titles)
+"""
+
+
+"""抓取二进制数据：图片、音频、视频
+r = requests.get('https://github.com/favicon.ico')  # 抓取github站点图标
+print(r.text)  # 直接输出str类型（图片直接转化为字符串）会出现乱码
+print(r.content)  # 返回bytes类型数据
+with open('favicon.ico', 'wb') as f:  # 以'wb'写二进制形式将bytes类型写入文件中
+    f.write(r.content)
+"""
+
+
+"""POST请求
+data = {
+    'name': 'germey',
+    'age': 22
+}
+r = requests.post('http://httpbin.org/post', data=data)  # 使用post()方法，提交数据
+print(r.text)  # forms部分就是提交的数据
+"""
+
+
+"""响应
+headers = {  # 构造请求头，伪装成浏览器，否则无法抓取
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+}
+r = requests.get('https://www.jianshu.com')
+print('Bad Request') if not r.status_code == requests.codes.ok else print('Request successfully')  # 内置状态码查询对象requests.codes，对比响应状态码是否是ok（即200）
+print(type(r.status_code), r.status_code)
+print(type(r.headers), r.headers)
+print(type(r.cookies), r.cookies)
+print(type(r.url), r.url)
+print(type(r.history), r.history)
+"""
+
+
+
