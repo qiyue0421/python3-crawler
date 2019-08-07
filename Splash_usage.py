7950bf3021f12c64c80d0d4799deb4da6148eb18
qiyue0421 <1831545391@qq.com>
2019-08-07 20:12:06 +0800

    Splash安装与下载

---
+++ Splash_usage.py
@@ -0,0 +1,24 @@
+# Splash是一个JavaScript渲染服务，是一个带有HTTP API的轻量级浏览器，同时它对接了Python中的Twisted和QT库，利用它可以实现动态渲染页面的抓取。
+
+"""1、功能介绍
+# Splash功能：
+# 异步方式处理多个网页渲染过程；
+# 获取渲染后的页面的源代码或截图；
+# 通过关闭图片渲染或者使用Adblock规则来加快页面渲染速度；
+# 可执行特定的JavaScript脚本；
+# 可通过Lua脚本来控制页面渲染过程；
+# 获取渲染的详细过程并通过HAR（HTTP Archive）格式呈现。
+"""
+
+
+"""2、准备工作
+
+# Docker安装教程
+# https://www.runoob.com/docker/windows-docker-install.html
+# （Docker Desktop for Windows桌面应用程序 https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe）
+# 安装Splash
+# docker run -p 8050:8050 scrapinghub/splash
+"""
+
+
+"""3、实例引入"""
