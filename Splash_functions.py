dfd76db013d5cbb84257d853577718e5d09c0b2b
qiyue0421 <1831545391@qq.com>
2019-08-10 19:17:38 +0800

    Splash对象的方法

--- Splash_functions.py
+++ Splash_functions.py
@@ -129,7 +129,7 @@
 """
 
 
-"""http_get()"""
+"""http_get()
 # 此方法可以模拟发送HTTP的GET请求
 #
 # 用法
@@ -141,63 +141,260 @@
 # follow_redirects：可选参数，表示是否启动自动重定向，默认为true
 #
 # 示例：
+# function main(splash, args)
+#     local treat = require("treat")
+#     local response = splash:http_get("http://httpbin.org/get")
+#     return {
+#         html = treat.as_string(response.body),
+#         url = response.url,
+#         status = response.status
+#     }
+# end
+"""
+
+
+"""http_post()
+# 和http_get()方法类似，此方法用来模拟发送POST请求，不过多了一个参数body
 #
-function main(splash, args)
-    local treat = require("treat")
-    local response = splash:http_get("http://httpbin.org/get")
-    return {
-        html = treat.as_string(response.body),
-        url = response.url,
-        status = response.status
-    }
-end
+# 用法
+# response = splash:http_post{url, headers=nil, follow_redirects=true, body=nil}
+#
+# 参数说明
+# url：请求URL
+# headers：可选参数，默认为空，请求头
+# follow_redirects：可选参数，表示是否启动自动重定向，默认为true
+# body：可选参数，即表单数据，默认为空
+#
+# 示例：
+# function main(splash, args)
+#     local treat = require("treat")
+#     local json = require("json")
+#     local response = splash:http_post{"http://httpbin.org/post",
+#                                 body = json.encode({name = "Germey"}),
+#                                 headers = {["content-type"] = "application/json"}
+#         }
+#     return {
+#         html = treat.as_string(response.body),
+#         url = response.url,
+#         status = response.status
+#     }
+# end
+"""
 
 
+"""set_content()
+# 此方法用来设置页面的内容
+#
+# 示例：
+# function main(splash)
+#     assert(splash:set_content("<html><body><h1>hello</h1></body></html>"))
+#     return splash:png()
+# end
+"""
 
 
+"""html()
+# 此方法用来获取网页的源代码，它是非常简单又常用的方法
+#
+# 示例：
+# function main(splash, args)
+#     splash:go("https://httpbin.org/get")
+#     return splash:html()
+# end
+"""
 
 
+"""png()
+# 此方法用来获取PNG格式的网页截图
+#
+# 示例：
+# function main(splash, args)
+#     splash:go("https://www.taobao.com")
+#     return splash:png()
+# end
+"""
 
 
+"""jpeg()
+# 此方法用来获取JPEG格式的网页截图
+# 
+# 示例：
+# function main(splash, args)
+#     splash:go("https://www.taobao.com")
+#     return splash:jpeg()
+# end
+"""
 
 
+"""har()
+# 此方法用来获取页面加载过程描述，其中显示了页面加载过程中每个请求记录的详情
+#
+# 示例：
+# function main(splash, args)
+#     splash:go("https://www.baidu.com")
+#     return splash:har()
+# end
+"""
 
 
+"""url()
+# 此方法可以获取当前正在访问的URL
+#
+# 示例：
+# function main(splash, args)
+#     splash:go("https://www.baidu.com")
+#     return splash:url()
+# end
+"""
 
 
+"""get_cookies()
+# 此方法可以获取当前页面的Cookies
+#
+# 示例：
+# function main(splash, args)
+#     splash:go("https://www.baidu.com")
+#     return splash:get_cookies()
+# end
+"""
 
 
+"""add_cookie()
+# 此方法可以为当前页面添加Cookie
+#
+# 用法
+# cookies = splash:add_cookie{name, value, path=nil, domain=nil, expires=nil, httpOnly=nil, secure=nil}
+# 各个参数代码Cookie的各个属性
+#
+# 示例：
+# function main(splash)
+#     splash:add_cookie{"sessionid", "237444131312", "/", domain="http://example.com"}
+#     splash:go("http://example.com/")
+#     return splash:html()
+# end
+"""
 
 
+"""clear_cookies()
+# 此方法可以清楚所有的Cookies
+#
+# 示例：
+# function main(splash)
+#     splash:go("http://www.baidu.com/")
+#     splash:clear_cookies()
+#     return splash:get_cookies()
+# end
+"""
 
 
+"""get_viewport_size()
+# 此方法可以获取当前浏览器页面的大小，即宽高
+#
+# 示例：
+# function main(splash)
+#     splash:go("https://www.baidu.com/")
+#     return splash:get_viewport_size()
+# end
+"""
 
 
+"""set_viewport_size()
+# 此方法可以设置当前浏览器页面的大小，即宽高
+#
+# 用法
+# splash:set_viewport_size(width, height)
+#
+# 示例：
+# function main(splash)
+#     splash:set_viewport_size(400, 700)
+#     assert(splash:go("https://cuiqingcai.com"))
+#     return splash:png()
+# end
+"""
 
 
+"""set_viewport_full()
+# 此方法可以设置浏览器全屏设置
+#
+# 示例：
+# function main(splash)
+#     splash:set_viewport_full()
+#     assert(splash:go("https://cuiqingcai.com"))
+#     return splash:png()
+# end
+"""
 
 
+"""set_user_agent()
+# 此方法可以设置浏览器的User-Agent
+#
+# 示例：
+# function main(splash)
+#     splash:set_user_agent('Splash')
+#     splash:go("https://httpbin.org/get")
+#     return splash:html()
+# end
+"""
 
 
+"""set_custom_headers()
+# 此方法可以设置请求头
+#
+# 示例：
+# function main(splash)
+#     splash:set_custom_headers({
+#         ["User-Agent"] = "Splash",
+#         ["Site"] = "Splash",
+#     })
+#     splash:go("http://httpbin.org/get")
+#     return splash:html()
+# end
+"""
 
 
+"""select()
+# 此方法可以选中符合条件的第一个节点，如果有多个节点符合条件，则只会返回一个，其参数是CSS选择器
+#
+# 示例：
+# function main(splash)
+#     splash:go("https://www.baidu.com/")  # 访问百度
+#     input = splash:select("#kw")  # 选中搜索框
+#     input:send_text('Splash')  # 调用send_text()方法填写了文本
+#     splash:wait(3)
+#     return splash:png()
+# end
+"""
 
 
+"""select_all()
+# 此方法可以选中所有符合条件的节点，其参数是CSS选择器
+#
+# 示例：
+# function main(splash)
+#     local treat = require('treat')
+#     assert(splash:go("http://quotes.toscrape.com/"))
+#     assert(splash:wait(0.5))
+#     local texts = splash:select_all('.quote .text')  # 通过CSS选择器选中了节点的正文内容
+#     local results = {}
+#     for index, text in ipairs(texts) do  # 遍历了所有节点，获取文本
+#         results[index] = text.node.innerHTML
+#     end
+#     return treat.as_array(results)
+# end
+"""
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""mouse_click()
+# 此方法可以模拟鼠标点击操作，传入的参数为坐标值x和y，此外，也可以直接选中某个节点，然后调用此方法
+#
+# 示例：
+# function main(splash)
+#     splash:go("http://www.baidu.com/")
+#     input = splash:select("#kw")
+#     input:send_text('Splash')
+#     submit = splash:select('#su')  # 选中“提交”按钮
+#     submit:mouse_click()  # 调用mouse_click()方法提交查询
+#     splash:wait(3)
+#     return splash:png()
+# end
+"""
